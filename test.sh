a6a75796ff230cdf95688a9cab11f74a555937c7
amit <gandhiamit04@gmail.com>
2018-08-07 15:59:48 +0530

    change in test script

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 echo "This is a test message" > /home/amit/Desktop/amit.gandhi/testing/test.log
 echo "Today's date is `date +%Y%m%d`" >> /home/amit/Desktop/amit.gandhi/testing/test.log
